038f4ac939a8653ab1633d86b853f8b330a1123a
GitOfK <1850402105@qq.com>
2023-06-27 18:42:38 +0800

    lesson 06 code part

--- l06/functional.rb
+++ l06/functional.rb
@@ -45,3 +45,44 @@ def functional_singles(states)
 end
 
 p functional_singles(states)
+
+numbers = 1..10
+
+# sum: Imperative solution
+def imperative_sum(numbers)
+  total = 0
+  numbers.each do |n|
+    total += n
+  end
+  total
+end
+
+p imperative_sum(numbers)
+
+# sum: Functional solution
+def functional_sum(numbers)
+  numbers.reduce { |total, n| total + n }
+end
+
+p functional_sum(numbers)
+
+# lengths: Imperative solution
+def imperative_lengths(states)
+  lengths = {}
+  states.each do |state|
+    lengths[state] = state.length
+  end
+  lengths
+end
+
+p imperative_lengths(states)
+
+# lengths: Functional solution
+def functional_lengths(states)
+  states.reduce({}) do |lengths, state|
+    lengths[state] = state.length
+    lengths
+  end
+end
+
+p functional_lengths(states)
